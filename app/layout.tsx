31fe8b07e058fe334e11957ebfd3c04930612b00
Josh Wootonn <joshuawootonn@gmail.com>
2023-02-14 17:27:48 -0600

    chore: migrate to experiment + add a font

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "./globals.css";
+
+import { Karla, Inconsolata } from "@next/font/google";
+
+const karla = Karla({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-karla",
+});
+
+const inconsolata = Inconsolata({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inconsolata",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${karla.variable} ${inconsolata.variable}`}>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
+      <main className="prose w-full max-w-2xl mx-auto mt-8 px-4 md:px-0">
         <h1>Better to Struggle Together</h1>
       </main>
     </>
