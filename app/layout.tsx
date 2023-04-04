20233602649943f549d0d4f484bca87dc8684252
Sasha Goloshchapov <31716295+ferrata@users.noreply.github.com>
2023-04-03 22:33:04 -0500

    feat: new UI (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,10 @@
-import { Karla, Inconsolata } from '@next/font/google'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 import './globals.css'
 
-const karla = Karla({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-karla',
-})
-
-const inconsolata = Inconsolata({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-inconsolata',
-})
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={`${karla.variable} ${inconsolata.variable}`}>
-            {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+        <html lang="en">
             <head />
             <body>{children}</body>
         </html>
